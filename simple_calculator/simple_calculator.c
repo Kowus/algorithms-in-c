075e9450ad94c0afc041ebcc87fdf82085c2cbf2
kowus <barnabas@elite-education.org>
2018-10-03 16:08:05 +0000

    loop

--- simple_calculator/simple_calculator.c
+++ simple_calculator/simple_calculator.c
@@ -4,38 +4,41 @@ int main(int argc, char const *argv[])
 {
   float firstNumber = 0.0f, secondNumber = 0.0f, result;
   int response;
-  printf("\n\nEnter 2 Numbers: ");
-  scanf("%f %f", &firstNumber, &secondNumber);
-  printf("\n\nWhat operation will you like to perform?\n [1] Add\t[2] Subtract\t[3] Multiply\t[4] Divide\t[5] Modulus\t");
-  scanf("%d", &response);
-
-  switch (response)
+  do
   {
-  case 1:
-    result = firstNumber + secondNumber;
-    printf("\n\nThe sum of %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
-    break;
-  case 2:
-    result = firstNumber - secondNumber;
-    printf("\n\nThe difference between %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
-    break;
-  case 3:
-    result = firstNumber * secondNumber;
-    printf("\n\nThe product %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
-    break;
-  case 4:
-    result = firstNumber / secondNumber;
-    printf("\n\nThe dividend of %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
-    break;
-  case 5:
-    result = (int)firstNumber % (int)secondNumber;
-    printf("\n\nThe modulus of %.0f and %.0f is %d\n\n", firstNumber, secondNumber, result);
-    break;
+    printf("\n\nEnter 2 Numbers: ");
+    scanf("%f %f", &firstNumber, &secondNumber);
+    printf("\n\nWhat operation will you like to perform?\n [1] Add\t[2] Subtract\t[3] Multiply\t[4] Divide\t[5] Modulus\t");
+    scanf("%d", &response);
 
-  default:
-    printf("\n\nOny3 soormi!!!\n\n");
-    break;
-  }
+    switch (response)
+    {
+    case 1:
+      result = firstNumber + secondNumber;
+      printf("\n\nThe sum of %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
+      break;
+    case 2:
+      result = firstNumber - secondNumber;
+      printf("\n\nThe difference between %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
+      break;
+    case 3:
+      result = firstNumber * secondNumber;
+      printf("\n\nThe product %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
+      break;
+    case 4:
+      result = firstNumber / secondNumber;
+      printf("\n\nThe dividend of %.0f and %.0f is %.0f\n\n", firstNumber, secondNumber, result);
+      break;
+    case 5:
+      result = (int)firstNumber % (int)secondNumber;
+      printf("\n\nThe modulus of %.0f and %.0f is %d\n\n", firstNumber, secondNumber, result);
+      break;
+
+    default:
+      printf("\n\nOny3 soormi!!!\n\n");
+      break;
+    }
+  } while (response != 0);
 
   return 0;
 }
