be8fb84bfe646ac80e8029170b82b54d85d1af95
kowus <barnabas@elite-education.org>
2018-10-17 15:15:45 +0000

    cofactor of matrices

--- matrices/main.c
+++ matrices/main.c
@@ -11,11 +11,12 @@ int main(int argc, char *argv[])
   printf("Please Enter the Number of Columns:\t");
   scanf("%d", &n);
 
+  printf("\n");
   for (i = 0; i < m; i++)
   {
     for (j = 0; j < n; j++)
     {
-      printf("\nPlease enter the element a[%d][%d]:\t", i, j);
+      printf("Please enter the element a[%d][%d]:\t", i, j);
       scanf("%d", &a[i][j]);
     }
   }
@@ -32,7 +33,25 @@ int main(int argc, char *argv[])
   i = 0;
   for (j = 0; j < n; j++)
     det = det + a[i][j] * (a[i + 1][(j + 1) % 3] * a[i + 2][(j + 2) % 3] - a[i + 1][(j + 2) % 3] * a[i + 2][(j + 1) % 3]);
-  printf("\nThe determinant: %d", det);
+  printf("\nThe determinant: %d\n", det);
 
+  for (i = 0; i < 3; i++)
+  {
+
+    for (j = 0; j < 3; j++)
+    {
+      c[i][j] = a[(i + 1) % 3][(j + 1) % 3] * a[(i + 2) % 3][(j + 2) % 3] - a[(i + 1) % 3][(j + 2) % 3] * a[(i + 2) % 3][(j + 1) % 3];
+    }
+  }
+
+  for (i = 0; i < m; i++)
+  {
+    printf("\n");
+    for (j = 0; j < n; j++)
+    {
+      printf("\t%d", c[i][j]);
+    }
+  }
+  printf("\n");
   return 0;
 }
