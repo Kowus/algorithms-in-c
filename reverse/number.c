a0529cfc5d2f640020dd10c34fd5c76c9885d4fe
kowus <barnabas@elite-education.org>
2018-12-09 20:01:14 +0000

    palindrome

--- reverse/number.c
+++ reverse/number.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
   int n;
   printf("Please enter the number you want to reverse:\t");
-  scanf("%d",&n);
-  int rev=0,rem;
+  scanf("%d", &n);
+  int rev = 0, rem;
   int num = n;
-  while(n>0)
+  while (n > 0)
   {
-      rem=n%10; //take out the remainder .. so it becomes 5 for 12345
-      rev=rev*10+rem; //multiply the current number by 10 and add this remainder.
-      n=n/10; //divide the number. So it becomes 1234.
+    rem = n % 10;         //take out the remainder .. so it becomes 5 for 12345
+    rev = rev * 10 + rem; //multiply the current number by 10 and add this remainder.
+    n = n / 10;           //divide the number. So it becomes 1234.
   }
   // printf("T%d",rev);
   printf("\nThe reverse of %d is %d, and their difference is %d", num, rev, num - rev);
+  if (num == rev)
+  {
+    printf("\n%d is a palindrome", num);
+  }
+  else
+  {
+    printf("\n%d is not a palindrome", num);
+  }
 }
